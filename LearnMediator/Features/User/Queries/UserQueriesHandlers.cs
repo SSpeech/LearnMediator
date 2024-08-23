8f43196e1c52d332f4961298a0f5f4baa085ad0e
Gonzalo Fernández <gonzo345@gmail.com>
2024-08-24 00:06:21 +0200

    FIX: Moved GetUsersListQuery to UserQueriesHandlers
    - Fixed with proper return content expected as well

--- LearnMediator/Features/User/Queries/UserQueriesHandlers.cs
+++ LearnMediator/Features/User/Queries/UserQueriesHandlers.cs
@@ -5,18 +5,21 @@ using LearnMediator.Repositories.UserRepository;
 
 namespace LearnMediator.Features.User.Queries
 {
-    public class UserQueryHandler : IQueryHandler<GetUserByIdQuery, Models.User?>
+    public class UserQueriesHandlers(FakeStoreData storeData) : IQueryHandler<GetUserByIdQuery, Models.User?>,
+                                                                IQueryHandler<GetUsersListQuery, IEnumerable<Models.User>>
     {
-        private readonly FakeStoreData _storeData;
-
-        public UserQueryHandler(FakeStoreData storeData) => _storeData = storeData;
-
         public async Task<Result<Models.User?>> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
         {
-            var user = await _storeData.GetUserById(request.Id);
+            var user = await storeData.GetUserById(request.Id);
             return user is null
                 ? Result.Failure<Models.User>(new Error("User.NotFound", $"The User with Id {request.Id} was not Found"))
                 : Result.Success(user);
+        }
+
+        public async Task<Result<IEnumerable<Models.User>>> Handle(GetUsersListQuery request, CancellationToken cancellationToken)
+        {
+            var item = await storeData.GetUsersAsync();
+            return Result.Success(item);
         }
     }
 }
